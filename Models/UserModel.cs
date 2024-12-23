ac04754f342b0042ae4ca783bb496069526d5de3
Sankalpa <joshisankalpa2@gmail.com>
2024-12-23 09:40:15 +0545

    initial required model class is created

---
+++ Models/UserModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WiseWallet.Models
+{
+    internal class UserModel
+    {
+        public Guid Id { get; set; }
+        public string? Gmail { get; set; }
+        public string? Password { get; set; }
+        public string? CurrencyType { get; set; }
+
+        public UserModel()
+        {
+            Id = Guid.NewGuid();
+            Gmail = string.Empty;
+            Password = string.Empty;
+            CurrencyType = string.Empty;
+        }
+    }
+}
